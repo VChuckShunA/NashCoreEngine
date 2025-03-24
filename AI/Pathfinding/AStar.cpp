77e20827b0045fc30c8ae8ddf02d5dc13a22b099
VChuckShunA <62978746+VChuckShunA@users.noreply.github.com>
2025-03-24 13:24:02 +0530

    Began Impelementing A Star

--- AI/Pathfinding/AStar.cpp
+++ AI/Pathfinding/AStar.cpp
@@ -1,1 +1,110 @@
 #include "AStar.h"
+#include <unordered_set>
+#include <iostream>
+void AStar::initializeNavMesh()
+{
+	navMesh.resize(NAVMESH_HEIGHT, std::vector<Node>(NAVMESH_WIDTH, Node({ 0,0 }, true)));
+
+	for (int x = 0; x < NAVMESH_WIDTH; x++)
+	{
+		for (int y = 0; y < NAVMESH_HEIGHT; y++)
+		{
+			navMesh[x][y] = Node({ x,y }, true);
+		}
+	}
+
+	//Marking obstactales
+	markObstacles();
+}
+
+void AStar::markObstacles()
+{
+
+	markObstacle(5,1);
+	markObstacle(5, 2);
+	markObstacle(5, 3);
+	markObstacle(5, 4);
+	markObstacle(5, 5);
+	markObstacle(5, 6);
+	markObstacle(5, 7);
+	markObstacle(5, 8);
+	markObstacle(5, 9);
+}
+
+void AStar::markObstacle(int x, int y)
+{
+	navMesh[x][y].walkable = false;
+}
+
+std::vector<Vec2> AStar::FindPath(Vec2 startPos, Vec2 endPos)
+{
+	Node* startNode = &navMesh[startPos.x][startPos.y];
+	Node* endNode = &navMesh[endPos.x][endPos.y];
+
+	std::vector<Node*>openList;
+	std::unordered_set<Node*> closedList;
+	
+	openList.push_back(startNode);
+
+	while (!openList.empty())
+	{
+		//Find the node with the lowest FCost
+		auto currentNode = *std::min_element(openList.begin(), openList.end(), [](Node* a, Node* b) {return a->fCost < b->fCost; });
+
+		//Remove it from the open list and add it to the closed list
+		openList.erase(std::remove(openList.begin(), openList.end(), currentNode), openList.end());
+		closedList.insert(currentNode);
+
+		//If we've reached the end node, reconstruct the path
+		if (currentNode == endNode)
+		{
+			std::vector<Vec2> path;
+			while (currentNode)
+			{
+				path.push_back(currentNode->position);
+				currentNode = currentNode->parent;
+			}
+			std::reverse(path.begin(), path.end());
+			for (Vec2 pathNode : path) {
+				std::cout << pathNode.x << " , " << pathNode.y<<std::endl;
+			}
+			return path;
+		}
+
+		//Process neighbors
+		std::vector<Vec2> directions = { {0,-1},{0,1},{-1,0},{1,0} };//Up,Down,Left,Right
+		for (auto& dir : directions)
+		{
+			Vec2 neighbourPos = currentNode->position + dir;
+			if (neighbourPos.x < 0 || neighbourPos.y < 0 || neighbourPos.x >= NAVMESH_WIDTH || neighbourPos.y >= NAVMESH_HEIGHT)
+			{
+				continue;
+			}
+
+			Node* neighbour = &navMesh[neighbourPos.x][neighbourPos.y];
+
+			if (!neighbour->walkable || closedList.count(neighbour))
+			{
+				continue;
+			}
+
+			int newGCost = currentNode->gCost + 1;
+			if (newGCost < neighbour->gCost || std::find(openList.begin(), openList.end(), neighbour) == openList.end())
+			{
+				neighbour->gCost = newGCost;
+				neighbour->CalculateCosts(endNode);
+				neighbour->parent = currentNode;
+				
+				if (std::find(openList.begin(), openList.end(), neighbour) == openList.end())
+				{
+					openList.push_back(neighbour);
+				}
+			}
+		}
+
+	}
+
+	//no path found
+	return {};
+	//std::vector<Vec2>();
+}
--- AI/Pathfinding/AStar.h
+++ AI/Pathfinding/AStar.h
@@ -1,5 +1,32 @@
 #pragma once
+#include "../../vec2.h"
+#include <cstdlib>
+#include <vector>
 class AStar
 {
+public:
+	struct Node {
+		Vec2 position;
+		int gCost, hCost, fCost;
+		bool walkable;
+		Node* parent;
+
+		Node(Vec2 pos,bool isWalkable) : position(pos),gCost(0),hCost(0),walkable(isWalkable),parent(nullptr){}
+		void CalculateCosts(Node* endNode)
+		{
+			hCost = std::abs(position.x - endNode->position.x) + std::abs(position.y - endNode->position.y);
+			fCost = gCost + hCost;
+		}
+	};
+
+	const int NAVMESH_WIDTH = 20, NAVMESH_HEIGHT = 12;
+	std::vector<std::vector<Node>> navMesh;
+
+	void initializeNavMesh();
+	void markObstacles();
+	void markObstacle(int x, int y);
+
+	std::vector<Vec2>FindPath(Vec2 startPos, Vec2 endPos);
+
 };
 
